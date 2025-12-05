511e0db2047d96787ea4591aa10ceeb3c0f79921
Thomas Clark <tnc1997@outlook.com>
2025-12-05 18:00:53 +0000

    feat(home): add cards to home screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'companies_card.dart';
+import 'questions_card.dart';
+import 'welcome_card.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
     super.key,
@@ -9,6 +13,21 @@ class HomeScreen extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            spacing: 16.0,
+            children: [
+              WelcomeCard(),
+              CompaniesCard(),
+              QuestionsCard(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
