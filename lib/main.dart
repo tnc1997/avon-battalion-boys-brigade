e7e6d1eecb82501722c8a9825d2838df91381ded
Thomas Clark <tnc1997@outlook.com>
2025-12-05 17:02:21 +0000

    refactor: update theme color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xff224b8e),
+          brightness: Brightness.light,
+          dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
