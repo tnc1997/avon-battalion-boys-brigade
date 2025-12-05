57d946fc5e7fe416540a33a3220661ff274a5ae5
Thomas Clark <tnc1997@outlook.com>
2025-12-05 17:58:01 +0000

    feat(home): add companies card

---
+++ lib/home/companies_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class CompaniesCard extends StatefulWidget {
+  const CompaniesCard({
+    super.key,
+  });
+
+  @override
+  State<CompaniesCard> createState() {
+    return _CompaniesCardState();
+  }
+}
+
+class _CompaniesCardState extends State<CompaniesCard> {
+  final _recognizer = TapGestureRecognizer();
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return Card(
+      margin: EdgeInsets.zero,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 16.0,
+          children: [
+            Text(
+              'Companies',
+              style: TextTheme.of(context).titleLarge,
+            ),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text:
+                        'We have a number of companies throughout the Bristol area, see them all ',
+                  ),
+                  TextSpan(
+                    text: 'here',
+                    style: TextStyle(
+                      color: ColorScheme.of(context).primary,
+                    ),
+                    recognizer: _recognizer,
+                  ),
+                  TextSpan(
+                    text: '.',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _recognizer.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _recognizer.onTap = () {};
+  }
+}
