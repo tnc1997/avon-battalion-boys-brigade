a901b9d7c4d2ebaca82c820de4c49de27ac51714
Thomas Clark <tnc1997@outlook.com>
2025-12-05 17:06:52 +0000

    refactor: add router to material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'home/home_route.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: GoRouter(
+        routes: [
+          $homeRoute,
+        ],
+      ),
       title: 'Avon Battalion Boys\' Brigade',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
           seedColor: Color(0xff224b8e),
           brightness: Brightness.light,
@@ -41,7 +37,6 @@ class MyApp extends StatelessWidget {
           dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
